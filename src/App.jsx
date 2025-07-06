1a5acfcebd93d84655dcf7f8bd8bb44526766418
TeamDelta1035 <meetd6553@gmail.com>
2025-07-06 09:57:29 +0530

    login init update done

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AppRouter from './routes/Router.jsx'
 
 function App() {
   return (
     <>
+      <AppRouter />
       <h1>Test The ReGreen</h1>
     </>
   )
