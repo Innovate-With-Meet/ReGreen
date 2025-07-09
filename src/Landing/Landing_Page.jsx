f4628a1f52049ffd3c7138a23685a4b5572669c3
Innovate-With-Meet <102943643+Innovate-With-Meet@users.noreply.github.com>
2025-07-09 11:21:18 +0530

    dependency installation update

--- src/Landing/Landing_Page.jsx
+++ src/Landing/Landing_Page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const LandingPage = () => {
     return (
         <>
-            <h1> Landing Page
+            <h1> User Dashboard
             </h1>
         </>
     )
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,22 +1,3 @@
-// import { Routes, Route } from "react-router-dom";
-// import Home from "../pages/Home";
-// import Login from "../pages/Login";
-// import Register from "../pages/Register";
-// import Dashboard from "../pages/Dashboard";
-// import NotFound from "../pages/NotFound";
-
-// export default function AppRouter() {
-//     return (
-//         <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/login" element={<Login />} />
-//             <Route path="/register" element={<Register />} />
-//             <Route path="/dashboard" element={<Dashboard />} />
-//             <Route path="*" element={<NotFound />} />
-//         </Routes>
-//     );
-// }
-
 import { Routes, Route } from "react-router-dom";
 import Login from "../User_Based_Login/Login.jsx";
 import Home from "../User_Based_Login/Home.jsx";
